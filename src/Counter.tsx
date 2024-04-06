b76f57288bcb21bda2172b3be02c2a595dccd86f
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-06 16:47:20 +0300

    Create button click events

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,15 @@
 const Counter = () => {
+    const handleAttack = () => {
+        alert('Attack is clicked!')
+    }
+    const handleDefence = () => {
+        alert('Defend is clicked!')
+    }
   return (
     <div className="row text-white">
         <h1>Counter: </h1>
-        <button style={{width:'200px'}}>+1</button>
-        <button style={{width:'200px'}}>-1</button>
+        <button onClick={handleAttack} style={{width:'200px'}}>+1</button>
+        <button onClick={handleDefence} style={{width:'200px'}}>-1</button>
     </div>
     );
 };
