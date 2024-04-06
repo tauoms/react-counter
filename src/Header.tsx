3dedb4bf4a6fdc698c936fe6a04a300bacee07f5
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-06 16:36:36 +0300

    Add styles to Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,14 @@
 import {FC} from 'react';
-
+import logo from './assets/react.svg';
 interface Props {}
 
 const Header: FC<Props> = () => {
   return (
-    <div>Header</div>
-  )
+    <div className='py-3 px-2' style={{borderBottom: '2px solid #777'}}>
+        <img src={logo} alt="logo" style={{ height: '60px', verticalAlign: 'top' }} />
+        <span className='h2 pt-4 m-2 text-white'>React Counter</span>
+    </div>
+    );
 };
 
 export default Header;
