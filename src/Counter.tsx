f5c5e7c09b1ee702aede9a541453b9e2bf77555c
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-06 16:44:01 +0300

    Add buttons to Counter

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,7 +1,11 @@
 const Counter = () => {
   return (
-    <div>Counter</div>
-  )
+    <div className="row text-white">
+        <h1>Counter: </h1>
+        <button style={{width:'200px'}}>+1</button>
+        <button style={{width:'200px'}}>-1</button>
+    </div>
+    );
 };
 
 export default Counter;
